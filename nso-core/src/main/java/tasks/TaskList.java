11aacd1a504fca1481dbcac11ee93413eab804b4
moonsmile-dev <kakavip.198.kaka@gmail.com>
2021-12-30 13:20:15 +0700

    fix: bypass nhat dia do

--- nso-core/src/main/java/tasks/TaskList.java
+++ nso-core/src/main/java/tasks/TaskList.java
@@ -587,6 +587,7 @@ public class TaskList {
                                                 .counts(new int[] {
                                                                 -1, 1, -1
                                                 })
+                                                .bypass(true)
                                                 .build(),
                                 builder()
                                                 .taskId(24)
