156a330cbc1a7e6ecd9178fa1f738135182a9a4c
moonsmile-dev <kakavip.198.kaka@gmail.com>
2022-04-13 10:00:22 +0700

    fix: update back data.

--- nso-core/src/main/java/boardGame/Place.java
+++ nso-core/src/main/java/boardGame/Place.java
@@ -4165,9 +4165,12 @@ public class Place {
         }
         for (byte l = 0; l < p.nj.get().ItemBody.length; ++l) {
             final Item item = p.nj.get().ItemBody[l];
-            if (item != null && item.isExpires && item.isExpired()) {
-                p.removeOrMoveBackNewItem(item);
-                p.nj.removeItemBody(l);
+            if (item != null) {
+                if (item.isExpires) {
+                    if (item.isExpired()) {
+                        p.nj.removeItemBody(l);
+                    }
+                }
             }
         }
         for (byte l = 0; l < p.nj.ItemBox.length; ++l) {
